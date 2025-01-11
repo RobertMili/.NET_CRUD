4c6380bd8335d143beba7c1066acfa2302df1fb1
RobertMili <robertmilicevic3869@gmail.com>
2025-01-11 10:29:36 +0100

    Update WebApplication1 and related test files

--- WebApplication1.Tests/obj/Debug/net8.0/WebApplication1.Tests.AssemblyInfo.cs
+++ WebApplication1.Tests/obj/Debug/net8.0/WebApplication1.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5288a755ec94159d3e527bb5d572d9387abdc75b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8083f8d3179375d642491b35f342243031a4f83")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
